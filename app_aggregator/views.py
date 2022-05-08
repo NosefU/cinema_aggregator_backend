8f5d79c281ebe9b155a8ef9e447b95f92e0fa566
Valko Dmitry <nosefu@gmail.com>
2022-05-08 16:53:54 +0300

    feat(views, templates) реализован селектор дат и вывод текущей выбранной даты
    если сеансов на дату из селектора нет, то кнопка выключается

--- app_aggregator/views.py
+++ app_aggregator/views.py
@@ -14,17 +14,19 @@ class SessionsList(View):
         req_city = request.GET.get('city', 'Белгород')
 
         try:
-            date_range = (
-                dt.datetime.strptime(req_date, '%Y-%m-%d'),
-                dt.datetime.strptime(req_date, '%Y-%m-%d') + dt.timedelta(hours=24)
-            )
+            selected_date = dt.datetime.strptime(req_date, '%Y-%m-%d')
         except ValueError:
             return HttpResponseBadRequest('Invalid date format')
+
+        date_range = (
+            selected_date,
+            selected_date + dt.timedelta(hours=24)
+        )
 
         queryset = Session.objects.filter(datetime__range=date_range, theater__city=req_city)\
             .order_by('movie_id', 'theater_id', 'datetime')
 
-        # формируем словарь вида
+        # формируем словарь сеансов вида
         # [{"movie": movie_obj, "theaters": [{"theater": theater_obj, "sessions": [session1_obj, session2_obj]}]}]
         objects = []
         prev_movie_id = None
@@ -48,10 +50,25 @@ class SessionsList(View):
             if session.datetime < current_time:
                 closed_sessions.append(session.id)
 
+        # для селектора дат на неделю вперёд формируем словарь вида {dt.date: bool},
+        # где значение элемента словаря - это доступность хотя бы одного сеанса в этот день
+        date_range = (
+            dt.date.today(),
+            dt.date.today() + dt.timedelta(days=7)
+        )
+        avail_dates = Session.objects.values_list('datetime', flat=True)\
+            .filter(datetime__range=date_range, theater__city=req_city)\
+            .order_by('datetime')
+
+        avail_dates = set([d.date() for d in avail_dates])
+        avail_dates = {d: d in avail_dates for d in [dt.date.today() + dt.timedelta(days=i) for i in range(7)]}
+
         context = {
             'object_list': objects,
-            'closed_sessions': closed_sessions
+            'closed_sessions': closed_sessions,
+            'avail_dates': avail_dates,
+            'selected_date': selected_date.date(),
+            'selected_city': req_city
         }
+
         return render(request, 'app_aggregator/sessions_list.html', context=context)
-
-
